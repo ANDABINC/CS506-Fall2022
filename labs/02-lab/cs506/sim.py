f748c681b1ca299a0e934ce78fe5e3f7bd40c99e
Andrew Choi <dooby@bu.edu>
2022-09-22 18:07:54 -0400

    Lab2
    work for lab2

--- labs/02-lab/cs506/sim.py
+++ labs/02-lab/cs506/sim.py
@@ -5,12 +5,24 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
+    # raise NotImplementedError()
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    bottom = 0
+    top = 0
+    for i in range(len(x)):
+        if x[i]==y[i]:
+            top+=1
+        bottom +=1
+    return 1-(abs(top) / abs(bottom))
+    # raise NotImplementedError()
 
 def cosine_sim(x, y):
+    
     raise NotImplementedError()
 
 # Feel free to add more
